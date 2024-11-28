4c818435d8f08f36472fffa0615e0edc1556b7b2
Maesawa-code <hirokitechcamp@gmail.com>
2024-11-28 15:54:46 +0900

    preview.js を新規・編集ページ限定に設定し、画像プレビュー領域を追加

---
+++ app/javascript/preview.js
@@ -0,0 +1,8 @@
+document.addEventListener('turbo:load', function(){
+  const postForm = document.getElementById('new_post');
+
+  const previewList = document.getElementById('previews');
+
+  if (!postForm) return null;
+  console.log("preview.jsが読み込まれました");
+});
